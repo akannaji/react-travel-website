c5b35c12b0359d08d19dbc99af632c52668176a2
Aka Nnaji <akamonye.nnaji@gmail.com>
2023-07-29 20:12:22 -0500

    fixed relative pathing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
-import Footer from './components/Footer';
 
 function App() {
   return (
